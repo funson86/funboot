72e6e29ff0a4550f0699726907a476c96b12636e
funson86 <funson86@gmail.com>
2021-05-11 16:07:14 +0800

    default

--- common/components/bbs/CacheSystemBbs.php
+++ common/components/bbs/CacheSystemBbs.php
@@ -5,6 +5,7 @@ namespace common\components\bbs;
 use common\components\base\CacheSystem;
 use common\helpers\ArrayHelper;
 use common\models\bbs\Node;
+use common\models\bbs\Tag;
 use common\models\mall\Category;
 use Yii;
 
@@ -31,7 +32,7 @@ class CacheSystemBbs extends CacheSystem
     /**
      * @return bool
      */
-    public function clearAllCategory()
+    public function clearAllNode()
     {
         return Yii::$app->cache->delete('bbsAllNode');
     }
@@ -46,30 +47,26 @@ class CacheSystemBbs extends CacheSystem
     public function getStoreNode($storeId = null, $parentId = null, $all = false, $isArray = false)
     {
         $storeId = is_null($storeId) ? Yii::$app->storeSystem->getId() : $storeId;
-        $data = false; //Yii::$app->cache->get('bbsStoreNode:' . $storeId);
-        if (!$data) {
-            $allNode = $this->getAllNode();
-            $list = [];
-            foreach ($allNode as $node) {
-                if ($node->store_id == $storeId) {
-                    if (is_null($parentId)) {
-                        if ($all || (!$all && $node->status == Node::STATUS_ACTIVE)) {
-                            $list[] = $isArray ? $node->attributes : $node;
-                        }
-                    } else {
-                        if ($node->parent_id == $parentId) {
-                            if ($all || (!$all && $node->status == Node::STATUS_ACTIVE)) {
-                                $list[] = $isArray ? $node->attributes : $node;;
-                            }
+
+        $models = $this->getAllNode();
+        $list = [];
+        foreach ($models as $model) {
+            if ($model->store_id == $storeId) {
+                if (is_null($parentId)) {
+                    if ($all || (!$all && $model->status == Node::STATUS_ACTIVE)) {
+                        $list[] = $isArray ? $model->attributes : $model;
+                    }
+                } else {
+                    if ($model->parent_id == $parentId) {
+                        if ($all || (!$all && $model->status == Node::STATUS_ACTIVE)) {
+                            $list[] = $isArray ? $model->attributes : $model;
                         }
                     }
                 }
             }
-
-            $data = $list;
-            // Yii::$app->cache->set('bbsStoreNode:' . $storeId, $data);
         }
-        return $data;
+
+        return $list;
     }
 
     /**
@@ -81,4 +78,57 @@ class CacheSystemBbs extends CacheSystem
         return Yii::$app->cache->delete('bbsStoreNode:' . $storeId);
     }
 
+    /**
+     * @return array|mixed|\yii\db\ActiveRecord[]
+     */
+    public function getAllTag()
+    {
+        $data = Yii::$app->cache->get('bbsAllTag');
+        if (!$data) {
+            $data = Tag::find()->orderBy(['sort' => SORT_ASC, 'id' => SORT_ASC])->all();
+            Yii::$app->cache->set('bbsAllTag', $data);
+        }
+        return ArrayHelper::mapIdData($data);
+    }
+
+    /**
+     * @return bool
+     */
+    public function clearAllTag()
+    {
+        return Yii::$app->cache->delete('bbsAllTag');
+    }
+
+
+    /**
+     * @param $storeId
+     * @param null $parentId
+     * @param bool $all
+     * @param bool $isArray
+     * @return array|mixed|\yii\db\ActiveRecord[]
+     */
+    public function getStoreTag($storeId = null, $parentId = null, $all = false, $isArray = false)
+    {
+        $storeId = is_null($storeId) ? Yii::$app->storeSystem->getId() : $storeId;
+
+        $models = $this->getAllTag();
+        $list = [];
+        foreach ($models as $model) {
+            if ($model->store_id == $storeId) {
+                if (is_null($parentId)) {
+                    if ($all || (!$all && $model->status == Tag::STATUS_ACTIVE)) {
+                        $list[] = $isArray ? $model->attributes : $model;
+                    }
+                } else {
+                    if ($model->parent_id == $parentId) {
+                        if ($all || (!$all && $model->status == Tag::STATUS_ACTIVE)) {
+                            $list[] = $isArray ? $model->attributes : $model;
+                        }
+                    }
+                }
+            }
+        }
+
+        return $list;
+    }
 }
