7e1c802866360a03aca08e2c1d8b370672ea0647
funson86 <funson86@gmail.com>
2021-05-26 18:04:46 +0800

    common asset

---
+++ common/components/assets/FlagIconCssAsset.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace common\components\assets;
+
+/**
+ * Fontawesome
+ * Class AnimateAsset
+ * @package common\widgets\echarts
+ * @author funson86 <funson86@gmail.com>
+ */
+class FlagIconCssAsset extends \yii\web\AssetBundle
+{
+    public $css = [
+        'https://cdn.jsdelivr.net/npm/flag-icon-css@3.5.0/css/flag-icon.min.css',
+    ];
+}
