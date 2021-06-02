91882baa1b1655839ef6f822b801c86703da5f89
funson86 <funson86@gmail.com>
2021-06-02 17:59:48 +0800

    master cms base

--- web/resources/js/cms_base.js
+++ web/resources/js/cms_base.js
@@ -1,10 +1,31 @@
-// bindtop
+// 滚动到地方再动画
+try {
+    new WOW().init();
+} catch (e) {
+    console.log(e);
+}
 
+// 延迟加载
+try {
+    lazyload();
+} catch (e) {
+    console.log(e);
+}
+
+// 大于700显示
 onscroll = function() {
     var st = document.documentElement.scrollTop || document.body.scrollTop;
-    if (!bind.offsetWidth && st >= 700) {
-        bind.style.display = 'block';
-    } else if (!!bind.offsetWidth && st < 700) {
-        bind.style.display = 'none';
+    var goTop = document.getElementById('goTop');
+    if (!goTop.offsetWidth && st >= 700) {
+        goTop.style.display = 'block';
+    } else if (!!goTop.offsetWidth && st < 700) {
+        goTop.style.display = 'none';
     }
 }
+
+// 平滑滑动到顶部
+jQuery(function ($) {
+    $(document).on("click", "#goTop", function () {
+        $('html,body').animate({scrollTop: '0px'}, 800);
+    })
+})
