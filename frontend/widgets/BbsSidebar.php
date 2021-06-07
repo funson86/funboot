de644bb3293625c700c2abac93c60c4b89e3afb3
funson86 <funson86@gmail.com>
2021-06-07 11:24:59 +0800

    m grade
    # Conflicts:
    #	frontend/widgets/BbsSidebar.php

--- frontend/widgets/BbsSidebar.php
+++ frontend/widgets/BbsSidebar.php
@@ -26,7 +26,7 @@ class BbsSidebar extends \yii\base\Widget
     {
         // $todayStart = mktime(0, 0, 0, date('m', $now), date('d', $now), date('Y', $now);
         $category = Topic::find()->where(['store_id' => Yii::$app->storeSystem->getId()])->andWhere(['>', 'category', 0])->orderBy(['category' => SORT_ASC, 'tag_id' => SORT_ASC, 'created_at' => SORT_DESC])->all();
-        $excellent = Topic::find()->where(['store_id' => Yii::$app->storeSystem->getId()])->andWhere(['>', 'grade', 0])->orderBy(['sort' => SORT_ASC, 'created_at' => SORT_DESC])->limit(8)->all();
+        $excellent = Topic::find()->where(['store_id' => Yii::$app->storeSystem->getId()])->andWhere(['grade' => Topic::GRADE_EXCELLENT])->orderBy(['sort' => SORT_ASC, 'created_at' => SORT_DESC])->limit(8)->all();
         return $this->render(Inflector::camel2id(StringHelper::basename(get_class($this))), [
             'type' => $this->type,
             'nodeId' => $this->nodeId,
