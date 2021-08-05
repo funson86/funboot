53917933e432e53ab3b9bfaf919916be2923e5f4
funson86 <funson86@gmail.com>
2021-08-05 16:56:03 +0800

    bug fix m

--- common/config/params.php
+++ common/config/params.php
@@ -159,8 +159,8 @@ return [
     'bbs' => [
         'managerRoleIds' => [50, 106], //管理员role id
         'indexNodeChildren' => [ // 主页二级栏目
-            ['id' => 0, 'name' => Yii::t('app', 'All')],
-            ['id' => 1, 'name' => Yii::t('app', 'Job')],
+            ['id' => 0, 'name' => 'All'],
+            ['id' => 1, 'name' => 'Job'],
         ],
     ],
 
