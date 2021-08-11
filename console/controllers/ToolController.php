68b6d39cbaeee8e3ad38b09e17e9df49656a640e
funson86 <funson86@gmail.com>
2021-08-11 18:54:25 +0800

    tool for once m

---
+++ console/controllers/ToolController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace console\controllers;
+
+/**
+ * Class ToolController
+ * @package console\controllers
+ * @author funson86 <funson86@gmail.com>
+ */
+class ToolController extends BaseController
+{
+    public function actionIndex()
+    {
+        echo 'tool';
+    }
+}
