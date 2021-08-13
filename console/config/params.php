99869699172143c7d08f99bd1cc9153407b0a2ee
funson86 <funson86@gmail.com>
2021-08-13 10:41:01 +0800

    chat config m

--- console/config/params.php
+++ console/config/params.php
@@ -5,4 +5,21 @@ return [
     'adminEmail' => 'admin@example.com',
 
     'webBaseUrl' => 'https://funboot.mayicun.com',
+
+    'chat' => [
+        'gateway' => [
+            'server' => '0.0.0.0',
+            'port' => '7272',
+            'lanIp' => '127.0.0.1', // 分布式部署时请设置成内网ip（非127.0.0.1）
+            'count' => 4, //进程数，gateway进程数建议与cpu核数相同
+        ],
+        'register' => [
+            'server' => '127.0.0.1', // 分布式部署时请设置成内网ip（非127.0.0.1）
+            'port' => '1236',
+        ],
+        'businessworker' => [
+            'count' => 4, //进程数，gateway进程数建议与cpu核数相同
+        ],
+    ]
+
 ];
--- console/modules/chat/controllers/ServerController.php
+++ console/modules/chat/controllers/ServerController.php
@@ -8,6 +8,7 @@ use GatewayWorker\Gateway;
 use GatewayWorker\Register;
 use Workerman\Worker;
 use yii\console\Controller;
+use Yii;
 
 /**
  * Class ServerController
@@ -27,13 +28,13 @@ class ServerController extends Controller
 
     public function actionGateway($global = false)
     {
-        $gateway = new Gateway("Websocket://0.0.0.0:7272");
+        $gateway = new Gateway("Websocket://" . (Yii::$app->params['chat']['gateway']['server'] ?? '0.0.0.0') . ":" . (Yii::$app->params['chat']['gateway']['port'] ?? '7272') . ")");
         // 设置名称，方便status时查看
         $gateway->name = 'ChatGateway';
         // 设置进程数，gateway进程数建议与cpu核数相同
-        $gateway->count = 4;
+        $gateway->count = Yii::$app->params['chat']['gateway']['count'] ?? 4;
         // 分布式部署时请设置成内网ip（非127.0.0.1）
-        $gateway->lanIp = '127.0.0.1';
+        $gateway->lanIp = Yii::$app->params['chat']['gateway']['lanIp'] ?? '127.0.0.1';
         // 内部通讯起始端口。假如$gateway->count=4，起始端口为2300
         // 则一般会使用2300 2301 2302 2303 4个端口作为内部通讯端口
         $gateway->startPort = 2300;
@@ -42,7 +43,7 @@ class ServerController extends Controller
         // 心跳数据
         $gateway->pingData = '{"type":"ping"}';
         // 服务注册地址
-        $gateway->registerAddress = '127.0.0.1:1236';
+        $gateway->registerAddress = (Yii::$app->params['chat']['register']['server'] ?? '127.0.0.1') . ':' . (Yii::$app->params['chat']['register']['port'] ?? '1236');
         Worker::runAll();
     }
 
@@ -52,9 +53,9 @@ class ServerController extends Controller
         // worker名称
         $worker->name = 'ChatBusinessWorker';
         // bussinessWorker进程数量
-        $worker->count = 4;
+        $worker->count = Yii::$app->params['chat']['businessworker']['count'] ?? 4;
         // 服务注册地址
-        $worker->registerAddress = '127.0.0.1:1236';
+        $worker->registerAddress = (Yii::$app->params['chat']['register']['server'] ?? '127.0.0.1') . ':' . (Yii::$app->params['chat']['register']['port'] ?? '1236');
 
         // 事件处理函数，需要实现onMessage onClose函数
         $worker->eventHandler = Events::class;
@@ -65,7 +66,7 @@ class ServerController extends Controller
 
     public function actionRegister($global = false)
     {
-        $register = new Register('text://0.0.0.0:1236');
+        $register = new Register('text://' . (Yii::$app->params['chat']['register']['server'] ?? '127.0.0.1') . ':' . (Yii::$app->params['chat']['register']['port'] ?? '1236'));
         Worker::runAll();
     }
 }
