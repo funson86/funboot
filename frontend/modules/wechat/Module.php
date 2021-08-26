b766a79eb36635db37e33955ecd4bed20f2e0a47
funson86 <funson86@gmail.com>
2021-08-26 19:14:06 +0800

    wechat

---
+++ frontend/modules/wechat/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace frontend\modules\wechat;
+
+/**
+ * wechat module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'frontend\modules\wechat\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
