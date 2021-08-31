a2bb1680910547613d33d5efec0f408090191a02
funson86 <funson86@gmail.com>
2021-08-31 17:14:46 +0800

    comment m

--- console/shell/queue.sh
+++ console/shell/queue.sh
@@ -1,5 +1,5 @@
-# 检查队列监听进程是否一直在  如果没有该进程，则后台执行
-# * * * * *  . /etc/profile; sh /www/funboot/console/queue.sh >> /home/centos/log/queue.log
+# crontab检查队列监听进程是否一直在  如果没有该进程，则后台执行
+# * * * * *  . /etc/profile; sh /www/funboot/console/shell/queue.sh >> /home/centos/log/queue.log
 
 #!/bin/sh
 ps -ef|grep 'funboot/yii queue/listen' |grep -v grep
