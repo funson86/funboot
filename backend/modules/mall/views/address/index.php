70fdfcd620130ce9b4910ecde60e2dfa838ba4f3
funson86 <funson86@gmail.com>
2022-02-25 19:50:26 +0800

    user_id

--- backend/modules/mall/views/address/index.php
+++ backend/modules/mall/views/address/index.php
@@ -39,7 +39,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
                         'id',
                         ['attribute' => 'store_id', 'visible' => $this->context->isAdmin(), 'value' => function ($model) { return $model->store->name; }, 'filter' => Html::activeDropDownList($searchModel, 'store_id', ArrayHelper::map($this->context->getStores(), 'id', 'name'), ['class' => 'form-control', 'prompt' => Yii::t('app', 'Please Filter')]),],
-                        'user_id',
+                        ['attribute' => 'user_id', 'value' => function ($model) { return $model->user->email; }, 'filter' => true],
                         // ['attribute' => 'name', 'format' => 'raw', 'value' => function ($model) { return Html::field('name', $model->name); }, 'filter' => true,],
                         'first_name',
                         'last_name',
