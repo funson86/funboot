e67e3a10f81185952eec41a3b356e7fcaf860a0d
funson86 <funson86@gmail.com>
2022-04-15 08:52:09 +0800

    optimize m

--- common/models/BaseModel.php
+++ common/models/BaseModel.php
@@ -74,25 +74,12 @@ class BaseModel extends ActiveRecord
      */
     public function behaviors()
     {
-        // 未登录认为是store管理员登录, console下不一定有Yii::$app->user
-        $userId = isset(Yii::$app->user) && !Yii::$app->user->getIsGuest() ? Yii::$app->user->id : Yii::$app->storeSystem->getUserId();
         return array_merge(parent::behaviors(), [
             TimestampBehavior::class,
             [
                 'class' => BlameableBehavior::class,
-                'attributes' => [
-                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_by', 'updated_by'],
-                    ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_by'],
-                ],
-                'value' => $userId,
+                'defaultValue' => Yii::$app->storeSystem->getUserId(),
             ],
-            /*[
-                'class' => TimestampBehavior::class,
-                'attributes' => [
-                    ActiveRecord::EVENT_BEFORE_INSERT => ['store_id'],
-                ],
-                'value' => Yii::$app->storeSystem->getId(),
-            ],*/
         ]);
     }
 
